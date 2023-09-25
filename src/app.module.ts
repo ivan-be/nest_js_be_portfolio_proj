22bf37f54b582a200b2d4701bd33b46ea01af34d
Pavlo Kuraksin <kuraksinpavel@gmail.com>
2023-09-25 19:41:18 +0300

    init nest postgres project with type-orm and nest.env

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as path from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: path.resolve(__dirname, '..', '.env'),
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('HOST_LINK'),
+        port: configService.get<number>('HOST_PORT'),
+        username: configService.get<string>('HOST_USERNAME'),
+        password: configService.get<string>('HOST_PASS'),
+        database: configService.get<string>('HOST_DB_NAME'),
+        synchronize: true,
+        logging: true,
+        autoLoadEntities: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+
+    UsersModule,
+  ],
+})
+export class AppModule {}
